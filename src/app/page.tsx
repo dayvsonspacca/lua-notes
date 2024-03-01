06fbe2b5909b2066ba6f17bdd9dc05b3b7d4c502
Dayvson Spacca <spacca.dayvson@gmail.com>
2024-03-01 18:13:14 -0300

    feat: start search note

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { NoteList } from "@/components/notes/notes-list";
 
 export default function Home() {
     return (
-        <main className="flex gap-4 mt-10 mx-3 my-2 h-full">
+        <main className="flex gap-4 mt-10 mx-3 my-2 h-full max-h-full">
             <NoteList />
             <NoteBlock />
         </main>
--- src/components/notes/notes-list.tsx
+++ src/components/notes/notes-list.tsx
@@ -1,25 +1,29 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { Button } from "../ui/button";
+import { Button } from "@/components/ui/button";
 import { HamburgerMenuIcon } from "@radix-ui/react-icons";
-import { ScrollArea } from "../ui/scroll-area";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { Input } from "@/components/ui/input";
 
 export function NoteList() {
     return (
-        <Card className="w-[16rem] h-full">
+        <Card className="w-[16rem]">
             <CardHeader className="p-4">
                 <CardTitle>My notes</CardTitle>
             </CardHeader>
-            <CardContent className="flex flex-col gap-2 p-4 max-h-full">
-                <ScrollArea className="w-full">
+            <CardContent className="px-4">
+                <ScrollArea className="w-full h-[26rem]">
                     {Array.from([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]).map(i => {
                         return (
-                            <Button key={i} className="flex items-center justify-start gap-3" variant="outline">
+                            <Button key={i} className="flex items-center justify-start gap-3 my-2 w-full" variant="outline">
                                 <HamburgerMenuIcon className="size-4 cursor-grab"/>
                                 Note {i}
                             </Button>
                         )
                     })}
                 </ScrollArea>
+                <div className="mt-4 flex">
+                    <Input placeholder="Search note..."/>
+                </div>
             </CardContent>
         </Card>
     );
